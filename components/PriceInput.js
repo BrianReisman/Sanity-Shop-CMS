a72edb26a46c1769af47c7a9ac075c34635419c7
BrianReisman <brianreisman@protonmail.com>
2021-06-18 12:59:51 -0400

    add custom price input

--- components/PriceInput.js
+++ components/PriceInput.js
@@ -2,16 +2,21 @@ import React from 'react';
 import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event';
 
 const createPatchFrom = (value) => {
-  console.log('hiii')
   // Even in the input field has a type of number, it still comes in as a string, so here we coerce it into the number datatype!
   return PatchEvent.from(value === '' ? unset(): set(Number(value)))
 }
+
+// Great way to format the money!
+const formatMoney = Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: "USD",
+}).format;
 
 // when sanity renders this out it passes props. They're giving you everything from the price schema
 const PriceInput = ({ type, value, onChange, inputComponent }) => {
   return (
     <div>
-      <h2>{type.title} - {value}</h2>
+      <h2>{type.title} - {value && formatMoney(value/100)}</h2>
       <p>{type.description}</p>
       <input
         type={type.name}
@@ -22,8 +27,8 @@ const PriceInput = ({ type, value, onChange, inputComponent }) => {
   )
 };
 
-// PriceInput.focus = function(){
-//   this._inputElement.focus()
-// }
+PriceInput.focus = function(){
+  this._inputElement.focus();
+}
 
 export default PriceInput
--- schemas/pizza.js
+++ schemas/pizza.js
@@ -1,5 +1,5 @@
 import { MdLocalPizza as icon } from "react-icons/md";
-import PriceInput from '../components/PriceInput'
+import PriceInput from "../components/PriceInput";
 
 export default {
   // computer name
@@ -42,31 +42,31 @@ export default {
       inputComponent: PriceInput,
     },
     {
-      name: 'toppings',
-      title: 'Toppings',
-      type: 'array', // since one to many
-      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+      name: "toppings",
+      title: "Toppings",
+      type: "array", // since one to many
+      of: [{ type: "reference", to: [{ type: "topping" }] }],
     },
   ],
   preview: {
     select: {
-      title: 'name',
-      media: 'image',
-      topping0: 'toppings.0.name',
-      topping1: 'toppings.1.name',
-      topping2: 'toppings.2.name',
-      topping3: 'toppings.3.name'
+      title: "name",
+      media: "image",
+      topping0: "toppings.0.name",
+      topping1: "toppings.1.name",
+      topping2: "toppings.2.name",
+      topping3: "toppings.3.name",
     },
-    prepare: ({title, media, ...tops}) => {
+    prepare: ({ title, media, ...tops }) => {
       // filter undefined toppings out
       // return the preview object for the pizza
-      const definedToppings = Object.values(tops).filter(Boolean)
+      const definedToppings = Object.values(tops).filter(Boolean);
 
       return {
         title,
         media,
-        subtitle: definedToppings.join(', ')
-      }
-    }
-  }
+        subtitle: definedToppings.join(", "),
+      };
+    },
+  },
 };
